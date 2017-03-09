bda6b32d54de3318d822b3eb912a2f2cd2f5b130
David Steinberg <david@resium.com>
2017-03-08 17:24:57 -0800

    Bump version number

--- python/ga4gh/schemas/ga4gh/peer_service_pb2.py
+++ python/ga4gh/schemas/ga4gh/peer_service_pb2.py
@@ -21,7 +21,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='ga4gh/schemas/ga4gh/peer_service.proto',
   package='ga4gh.schemas.ga4gh',
   syntax='proto3',
-  serialized_pb=_b('\n&ga4gh/schemas/ga4gh/peer_service.proto\x12\x13ga4gh.schemas.ga4gh\x1a ga4gh/schemas/ga4gh/common.proto\x1a*ga4gh/schemas/google/api/annotations.proto\"9\n\x10ListPeersRequest\x12\x11\n\tpage_size\x18\x01 \x01(\x05\x12\x12\n\npage_token\x18\x02 \x01(\t\"V\n\x11ListPeersResponse\x12(\n\x05peers\x18\x01 \x03(\x0b\x32\x19.ga4gh.schemas.ga4gh.Peer\x12\x17\n\x0fnext_page_token\x18\x02 \x01(\t\">\n\x13\x41nnouncePeerRequest\x12\'\n\x04peer\x18\x01 \x01(\x0b\x32\x19.ga4gh.schemas.ga4gh.Peer\"\\\n\x14\x41nnouncePeerResponse\x12\x0f\n\x07success\x18\x01 \x01(\x08\x12\x33\n\nattributes\x18\x02 \x01(\x0b\x32\x1f.ga4gh.schemas.ga4gh.Attributes\"\x10\n\x0eGetInfoRequest\"`\n\x0fGetInfoResponse\x12\x18\n\x10protocol_version\x18\x01 \x01(\t\x12\x33\n\nattributes\x18\x02 \x01(\x0b\x32\x1f.ga4gh.schemas.ga4gh.Attributes\"H\n\x04Peer\x12\x0b\n\x03url\x18\x01 \x01(\t\x12\x33\n\nattributes\x18\x02 \x01(\x0b\x32\x1f.ga4gh.schemas.ga4gh.Attributes2\xfa\x02\n\x0bPeerService\x12|\n\tListPeers\x12%.ga4gh.schemas.ga4gh.ListPeersRequest\x1a&.ga4gh.schemas.ga4gh.ListPeersResponse\" \x82\xd3\xe4\x93\x02\x1a\"\x15/v0.6.0a10/peers/list:\x01*\x12\x80\x01\n\x0c\x41nnouncePeer\x12(.ga4gh.schemas.ga4gh.AnnouncePeerRequest\x1a).ga4gh.schemas.ga4gh.AnnouncePeerResponse\"\x1b\x82\xd3\xe4\x93\x02\x15\"\x13/v0.6.0a10/announce\x12j\n\x04Info\x12#.ga4gh.schemas.ga4gh.GetInfoRequest\x1a$.ga4gh.schemas.ga4gh.GetInfoResponse\"\x17\x82\xd3\xe4\x93\x02\x11\x12\x0f/v0.6.0a10/infob\x06proto3')
+  serialized_pb=_b('\n&ga4gh/schemas/ga4gh/peer_service.proto\x12\x13ga4gh.schemas.ga4gh\x1a ga4gh/schemas/ga4gh/common.proto\x1a*ga4gh/schemas/google/api/annotations.proto\"9\n\x10ListPeersRequest\x12\x11\n\tpage_size\x18\x01 \x01(\x05\x12\x12\n\npage_token\x18\x02 \x01(\t\"V\n\x11ListPeersResponse\x12(\n\x05peers\x18\x01 \x03(\x0b\x32\x19.ga4gh.schemas.ga4gh.Peer\x12\x17\n\x0fnext_page_token\x18\x02 \x01(\t\">\n\x13\x41nnouncePeerRequest\x12\'\n\x04peer\x18\x01 \x01(\x0b\x32\x19.ga4gh.schemas.ga4gh.Peer\"\\\n\x14\x41nnouncePeerResponse\x12\x0f\n\x07success\x18\x01 \x01(\x08\x12\x33\n\nattributes\x18\x02 \x01(\x0b\x32\x1f.ga4gh.schemas.ga4gh.Attributes\"\x10\n\x0eGetInfoRequest\"t\n\x0fGetInfoResponse\x12\x18\n\x10protocol_version\x18\x01 \x01(\t\x12\x12\n\nremote_url\x18\x03 \x01(\t\x12\x33\n\nattributes\x18\x02 \x01(\x0b\x32\x1f.ga4gh.schemas.ga4gh.Attributes\"H\n\x04Peer\x12\x0b\n\x03url\x18\x01 \x01(\t\x12\x33\n\nattributes\x18\x02 \x01(\x0b\x32\x1f.ga4gh.schemas.ga4gh.Attributes2\xfa\x02\n\x0bPeerService\x12|\n\tListPeers\x12%.ga4gh.schemas.ga4gh.ListPeersRequest\x1a&.ga4gh.schemas.ga4gh.ListPeersResponse\" \x82\xd3\xe4\x93\x02\x1a\"\x15/v0.6.0a10/peers/list:\x01*\x12\x80\x01\n\x0c\x41nnouncePeer\x12(.ga4gh.schemas.ga4gh.AnnouncePeerRequest\x1a).ga4gh.schemas.ga4gh.AnnouncePeerResponse\"\x1b\x82\xd3\xe4\x93\x02\x15\"\x13/v0.6.0a10/announce\x12j\n\x04Info\x12#.ga4gh.schemas.ga4gh.GetInfoRequest\x1a$.ga4gh.schemas.ga4gh.GetInfoResponse\"\x17\x82\xd3\xe4\x93\x02\x11\x12\x0f/v0.6.0a10/infob\x06proto3')
   ,
   dependencies=[ga4gh_dot_schemas_dot_ga4gh_dot_common__pb2.DESCRIPTOR,ga4gh_dot_schemas_dot_google_dot_api_dot_annotations__pb2.DESCRIPTOR,])
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
@@ -213,7 +213,14 @@ _GETINFORESPONSE = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='attributes', full_name='ga4gh.schemas.ga4gh.GetInfoResponse.attributes', index=1,
+      name='remote_url', full_name='ga4gh.schemas.ga4gh.GetInfoResponse.remote_url', index=1,
+      number=3, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='attributes', full_name='ga4gh.schemas.ga4gh.GetInfoResponse.attributes', index=2,
       number=2, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
@@ -232,7 +239,7 @@ _GETINFORESPONSE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=464,
-  serialized_end=560,
+  serialized_end=580,
 )
 
 
@@ -269,8 +276,8 @@ _PEER = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=562,
-  serialized_end=634,
+  serialized_start=582,
+  serialized_end=654,
 )
 
 _LISTPEERSRESPONSE.fields_by_name['peers'].message_type = _PEER
